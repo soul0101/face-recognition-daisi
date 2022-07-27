6b263a15e284de39e8972a4a94ade813eef78430
soul0101 <53980340+soul0101@users.noreply.github.com>
2022-07-27 12:17:30 +0530

    multipage st test

--- main.py
+++ main.py
@@ -229,7 +229,6 @@ def face_detection_page():
 
 def face_recognition_page():
 
-    st.sidebar.header("Upload Image")
     st.title("Face Recognition")
 
     col1, col2= st.columns([1,1])
@@ -272,11 +271,4 @@ def face_recognition_page():
                 st.write("Authentication Failed!")
 
 if __name__ == "__main__":
-    page_names_to_funcs = {
-        "Introduction": intro,
-        "Face Detection": face_detection_page,
-        "Face Recognition": face_recognition_page
-    }
-
-    demo_name = st.sidebar.selectbox("Choose a demo", page_names_to_funcs.keys())
-    page_names_to_funcs[demo_name]()
+    intro()
