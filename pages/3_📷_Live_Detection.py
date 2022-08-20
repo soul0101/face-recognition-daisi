bff6eb27050070d0575f13907f2bc7679e6f5bd1
soul0101 <53980340+soul0101@users.noreply.github.com>
2022-08-20 12:06:51 +0530

    added ice servers

--- pages/3_📷_Live_Detection.py
+++ pages/3_📷_Live_Detection.py
@@ -2,7 +2,7 @@ import av
 import cv2
 import streamlit as st
 from main import get_face_locations_fast
-from streamlit_webrtc import webrtc_streamer
+from streamlit_webrtc import webrtc_streamer, RTCConfiguration, WebRtcMode
 
 st.title("Webcam Live Face Detection")
 
@@ -16,8 +16,20 @@ def find_faces(frame):
             
     return av.VideoFrame.from_ndarray(img, format="bgr24")
 
+RTC_CONFIGURATION = RTCConfiguration(
+    {"iceServers": [{"urls": ["stun:stun.l.google.com:19302"]}, 
+                    {"urls": ["stun:stun3.l.google.com:19302"]},
+                    {"urls": ["stun:stun.voipzoom.com:3478"]},
+                    {"urls": ["stun:stun.halonet.pl:3478"]},
+                    {"urls": ["stun:stun.altar.com.pl:347"]}
+                    ]
+    }
+)
+
 ctx = webrtc_streamer(
     key="object-detection",
+    mode=WebRtcMode.SENDRECV,
+    rtc_configuration=RTC_CONFIGURATION,
     media_stream_constraints={"video": True, "audio": False},
     video_frame_callback=find_faces,
     async_processing=True
